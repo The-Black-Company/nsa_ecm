04229adfe8a4da8fcf8320d54c8e9f6ca413b823
Elania <83014218+Elania-Marvers@users.noreply.github.com>
2022-11-25 10:21:34 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!bin/sh	
+
 sudo apt update
 sudo apt upgrade
 sudo apt install emacs-nox git vim tree htop curl emacs net-tools make unzip sudo python3 ca-certificates apt-transport-https software-properties-common python3-pip wget
-wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" |tee /etc/apt/sources.list.d/php.list
+
+wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+
+
 
 sudo apt install php8.0 php8.0-{cli,fpm,json,pdo,mysql,zip,gd,mbstring,curl,xml,pear,bcmath,intl} 
+
+
+
+echo "PermitRootLogin yes" > /etc/ssh/sshd_config
